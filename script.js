483bae70d85de903f44c413c99d37b5f04b79241
elyas-get <psudoelyas@gmail.com>
2025-09-11 22:02:19 +0300

    intial commit

--- script.js
+++ script.js
@@ -1,10 +1,35 @@
 const gameBoard = (function () {
   let board = ["", "", "", "", "", "", "", "", ""];
+  let turn = 0;
 
   const update = (value, position) => {
     if (board[position] === "") {
       board[position] = value;
     }
+  };
+
+  const p1 = player("Elyas", "X");
+  const p2 = player("Abebe", "O");
+
+  const makeMove = (position) => {
+    turn++;
+    const currentMarker = turn % 2 === 0 ? p1.marker : p2.marker;
+    gameBoard.update(currentMarker, position);
+    return currentMarker;
+  };
+
+  const click = () => {
+    const cells = document.querySelectorAll(".cell");
+
+    cells.forEach((cell, index) => {
+      cell.addEventListener("click", () => {
+        if (board[index] === "") {
+          const marker = makeMove(index);
+          cell.innerText = marker;
+          cell.classList.add(marker.toLowerCase());
+        }
+      });
+    });
   };
   const boardFull = () =>
     board.every((element) => {
@@ -16,7 +41,7 @@ const gameBoard = (function () {
     });
   const reset = () => (board = ["", "", "", "", "", "", "", "", ""]);
   const getBoard = () => [...board];
-  return { update, reset, getBoard, boardFull };
+  return { update, reset, getBoard, boardFull, click };
 })();
 
 function player(name, marker) {
@@ -41,3 +66,16 @@ function checkWinner(marker) {
     pattern.every((index) => b[index] === marker)
   );
 }
+
+function startGame() {
+  const startBtn = document.getElementById("startBtn");
+  const board = document.querySelector(".game-board");
+
+  startBtn.addEventListener("click", () => {
+    board.classList.remove("hidden");
+    startBtn.style.display = "none";
+    gameBoard.click();
+  });
+}
+
+startGame();
